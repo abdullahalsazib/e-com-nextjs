90dc68fc9a3ea9d5e81bacc607aaaf69d7aed3b1
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-05-19 00:24:27 +0600

    19_May_25:12:24AM

--- src/app/(root)/product-about/page.tsx
+++ src/app/(root)/product-about/page.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 
 //image import
 import paypal from "@/../public/paypal.svg";
+import product1 from "@/../public/product-image/image2.png";
+import zipTextLogo from "@/../public/ZipTextLogo.svg";
+import { GoChevronRight } from "react-icons/go";
+import { FaRegHeart } from "react-icons/fa6";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { VscMail } from "react-icons/vsc";
 
 const tab = [
   { title: "About Product" },
@@ -36,8 +42,60 @@ const page = () => {
           </div>
         </div>
         <div className=" w-full px-[10%] flex items-center justify-center">
-          <div className=" w-1/2 h-screen bg-[#F5F7FF]"></div>
-          <div className=" w-1/2 h-screen bg-[#000]"></div>
+          <div className=" w-1/2 bg-[#F5F7FF] flex items-center justify-end">
+            <div className=" w-[80%]  h-screen py-20  px-4">
+              <div className="py-5 text-xs font-semibold uppercase flex items-center justify-start gap-1 text-gray-800">
+                <p>Home</p>
+                <GoChevronRight className=" text-blue-500" />
+                <p>Products</p>
+                <GoChevronRight className=" text-blue-500" />
+                <p className=" font-light text-gray-600">MSI WS Series</p>
+              </div>
+              <div className=" ">
+                <h1 className=" text-5xl font-semibold capitalize py-5">
+                  MSI MPG Trident 3
+                </h1>
+                <a
+                  href="#"
+                  className="text-blue-500 text-xs underline underline-offset-1 hover:text-blue-700 duration-200"
+                >
+                  Be the first to review this product
+                </a>
+                <p className=" text-xl text-justify font-light uppercase mt-5 pr-5">
+                  MSI MPG Trident 3 10SC-005AU Intel i7 10700F, 2060 SUPER, 16GB
+                  RAM, 512GB SSD, 2TB HDD, Windows 10 Home, Gaming Keyboard and
+                  Mouse 3 Years Warranty Gaming Desktop
+                </p>
+                <div className=" mt-20 w-full flex items-center justify-between">
+                  <p className="font-bold">
+                    Have a Question?
+                    <a href="#" className="text-blue-500">
+                      Contact Us
+                    </a>
+                  </p>
+                  <p className="text-gray-600 uppercase">SKU D5515AI</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" w-1/2 py-20 h-screen bg-[#fff]">
+            <div className=" flex items-start justify-start">
+              <div className=" px-3 py-3 w-[50%] relative">
+                <div className=" absolute top-0 left-0 px-3 py-2 flex items-center justify-center flex-col gap-2">
+                  <IconBtn Icon={<FaRegHeart />} />
+
+                  <IconBtn
+                    Icon={<HiOutlineBars3BottomRight className=" rotate-90" />}
+                  />
+                  <IconBtn Icon={<VscMail />} />
+                </div>
+                <Image src={product1} alt="product image" className="m-auto" />
+              </div>
+            </div>
+            <div className=" px-10 py-10">
+              <Image src={zipTextLogo} alt="zip brand" />
+            </div>
+          </div>
         </div>
       </div>
     </>
@@ -45,3 +103,18 @@ const page = () => {
 };
 
 export default page;
+
+const IconBtn: React.FC<{
+  Icon: React.ReactNode;
+  className?: string;
+}> = ({ className, Icon }) => {
+  return (
+    <>
+      <button
+        className={` p-1 text-lg text-gray-500 border-2 border-slate-400 rounded-full ${className} hover:text-gray-800 duration-300 hover:border-gray-800 font-bold`}
+      >
+        {Icon}
+      </button>
+    </>
+  );
+};
