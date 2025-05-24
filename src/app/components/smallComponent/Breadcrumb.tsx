cfe053d36f45e1234668ff41abacb355c96e0e0d
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-05-24 20:28:30 +0600

    24_5_25

--- src/app/components/smallComponent/Breadcrumb.tsx
+++ src/app/components/smallComponent/Breadcrumb.tsx
@@ -3,31 +3,31 @@ import React from "react";
 import { GoChevronRight } from "react-icons/go";
 
 interface BreadcrumbProps {
-  items: {
-    label: string;
-    active?: boolean;
-  }[];
+    items: {
+        label: string;
+        active?: boolean;
+    }[];
 }
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ items }) => {
-  return (
-    <div className="py-3 text-xs font-semibold uppercase flex items-center justify-start gap-1 text-gray-800 flex-wrap">
-      {items.map((item, index) => (
-        <React.Fragment key={index}>
-          <p
-            className={`${
-              item.active ? "font-light text-gray-600" : "text-gray-800"
-            }`}
-          >
-            {item.label}
-          </p>
-          {index < items.length - 1 && (
-            <GoChevronRight className="text-blue-500" />
-          )}
-        </React.Fragment>
-      ))}
-    </div>
-  );
+    return (
+        <div className="py-3 text-xs font-semibold uppercase flex items-center justify-start gap-1 text-gray-800 flex-wrap">
+            {items.map((item, index) => (
+                <React.Fragment key={index}>
+                    <p
+                        // this p for show the content
+                        className={`${item.active ? " font-light text-gray-600" : " cursor-pointer text-gray-800"
+                            }`}
+                    >
+                        {item.label}
+                    </p>
+                    {index < items.length - 1 && (
+                        <GoChevronRight className="text-blue-500" />
+                    )}
+                </React.Fragment>
+            ))}
+        </div>
+    );
 };
 
 export default Breadcrumb;
